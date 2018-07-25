fc310cf74db0b0857f73b3faf5caff7a7e3051b1
Xiaoge <hou.xia@husky.neu.edu>
2018-07-24 21:42:08 -0400

    Initicalize project

---
+++ src/main/java/com/spring/boot/appcontinuum/appcontinuum/AppcontinuumApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot.appcontinuum.appcontinuum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcontinuumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcontinuumApplication.class, args);
+	}
+}
