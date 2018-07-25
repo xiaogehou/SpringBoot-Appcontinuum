d3b236c6c923c9490f1e4ad22a61f040a1419402
Xiaoge <hou.xia@husky.neu.edu>
2018-07-24 22:24:55 -0400

    Adding one service and register to application-server-one

--- applications/application-server-one/src/main/java/com/spring/boot/appcontinuum/applicationserverone/AppcontinuumApplication.java
+++ applications/application-server-one/src/main/java/com/spring/boot/appcontinuum/applicationserverone/AppcontinuumApplication.java
@@ -2,10 +2,11 @@ package com.spring.boot.appcontinuum.applicationserverone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("example.hello")
 public class AppcontinuumApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppcontinuumApplication.class, args);
 	}
