d3df6635a06d3edf47950c3ff9d7fdac8ba1ecb2
Xiaoge <hou.xia@husky.neu.edu>
2018-07-24 21:58:06 -0400

    restructure project to follow appcontinuum

--- applications/application-server-one/src/main/java/com/spring/boot/appcontinuum/applicationserverone/AppcontinuumApplication.java
+++ applications/application-server-one/src/main/java/com/spring/boot/appcontinuum/applicationserverone/AppcontinuumApplication.java
@@ -1,4 +1,4 @@
-package com.spring.boot.appcontinuum.appcontinuum;
+package com.spring.boot.appcontinuum.applicationserverone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- applications/application-server-one/src/test/java/com/spring/boot/appcontinuum/applicationserverone/AppcontinuumApplicationTests.java
+++ applications/application-server-one/src/test/java/com/spring/boot/appcontinuum/applicationserverone/AppcontinuumApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.boot.appcontinuum.appcontinuum;
+package com.spring.boot.appcontinuum.applicationserverone;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
